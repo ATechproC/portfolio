179a5ca47d862c24067873676e32beccd56b0e6b
ATechproC <choraichianass@gmail.com>
2025-09-01 11:26:39 +0200

    start services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import About from "../components/About";
-import Header from "../components/Header";
-import NavBar from "../components/NavBar";
+
 
 
 export default function Home() {
   return (
     <>
-      <NavBar />
-      <Header />
-      <About />
+      
     </>
   );
 }
