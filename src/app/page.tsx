bc4cabce2bfddddc4adea9f462088150f0909eb6
ATechproC <choraichianass@gmail.com>
2025-09-01 13:48:41 +0200

    create work section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import About from "../components/About";
-import Header from "../components/Header";
-import NavBar from "../components/NavBar";
-import Services from "../components/Services";
+import Work from "../components/Work";
 
 export default function Home() {
   return (
     <>
-      <NavBar />
-      <Header />
-      <About />
-      <Services />
+      <Work />
     </>
   );
 }
