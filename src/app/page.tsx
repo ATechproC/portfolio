c5d155aee0ba9f47c5b7b0bc95b7bb0f60ab5bed
ATechproC <choraichianass@gmail.com>
2025-09-01 11:12:15 +0200

    update develop branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import About from "../components/About";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
 
 
 export default function Home() {
   return (
     <>
+      <NavBar />
+      <Header />
       <About />
     </>
   );
