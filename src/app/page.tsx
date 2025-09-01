a7b4bfd184bd003bb952de3b5bac1dc1c14df71f
ATechproC <choraichianass@gmail.com>
2025-09-01 19:56:36 +0200

    update develop feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "../components/About";
+import Contact from "../components/Contact";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Services from "../components/Services";
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Services />
       <Work />
+      <Contact />
     </>
   );
 }
